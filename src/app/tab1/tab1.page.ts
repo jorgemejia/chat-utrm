c8f52dee8b7e9e6f8d118754b6771057f8703e9e
Jorg <jorge_mejia@outlook.com>
2022-03-18 09:24:55 -0500

    Update tab1.page.ts

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import { SocketServiceService } from '../services/socket-service.service';
 import {ActivatedRoute, Router} from '@angular/router';
-import {UserServiceService} from '../services/user-service.service';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-tab1',
@@ -28,20 +28,22 @@ export class Tab1Page implements OnInit {
   constructor(
     private socketService: SocketServiceService,
     private route: ActivatedRoute,
-    private usrService: UserServiceService,
-    private router: Router,
+    private service: UserService,
+    private router: Router
     ) {}
 
   ngOnInit() {
-    console.log('init tab');
-    this.userId = this.usrService.getId();
+
+    this.userId = this.service.getId();
+    // console.log('init tab');
+    // this.user = this.route.snapshot.queryParams;
     // console.log('init tab', this.user);
 
     // if(this.user){
     //   this.user.id = parseInt(this.user.id, 10);
     // }
 
-    console.log('user id in tab', this.userId);
+    console.log('user in tab', this.userId);
     this.socketService.getOnlineUsers().subscribe((users) => {
       console.log('users connected', users);
       if(users) {
@@ -50,8 +52,8 @@ export class Tab1Page implements OnInit {
     });
   }
 
-  async navigateToChat() {
-    await this.router.navigate(['/home/tab2']);
+  async navigateToChat(id) {
+    await this.router.navigate(['/home/tab2'], { queryParams: { userId: id } } );
   }
 
   hola(){
